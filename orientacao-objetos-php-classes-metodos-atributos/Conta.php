8e7f14aa70684d021c413aef99f32217cdb7d1a5
RaissaKestring <raissakestring23@gmail.com>
2024-05-03 15:16:02 -0300

    Uso de this em métodos

---
+++ orientacao-objetos-php-classes-metodos-atributos/Conta.php
@@ -0,0 +1,43 @@
+<?php
+
+// classe que a partir dela crio objetos e mont meu sistema
+// deixa de ser orientada a funções, e passa a ser por objetos
+// hierarquia: classe -> atributos, objeto -> valores
+// com uma classe crio varios objetos
+// classe: forma de bolo
+// objeto: bolo já jeito
+
+class Conta  {
+
+    // classes são formas de bolo, planta de casa, então eu nãodefino os valores desas variáveis na classe, mas sim nos objetos
+
+    // definir dados da conta
+    // atributos de classe: 
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public float $saldo = 0;
+
+    // THIS: referencia membros de uma classe dentro dos métodos dessa classe.
+    // altero o valor do saldo que chamou a conta, e não todas 
+    // funnção dentro de classe: método
+    public function sacar(float $valorASacar) {
+
+        if($valorASacar > $this->saldo) {
+            echo "Saldo indisponivel";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void {
+        
+        if($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+        } else {
+            $this->saldo += $valorADepositar;
+        }
+    }
+
+    // não é necessário especificar void para métodos que não retornam valores. Se você omitir o tipo de retorno, o PHP assumirá implicitamente que o método não retorna nada.
+
+}
